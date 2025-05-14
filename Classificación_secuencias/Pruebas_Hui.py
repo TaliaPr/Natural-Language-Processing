7a879b3b55e112d2c78ae0fb1e92e908d8f1c1a9
Hui <155244905+Jiahuiche@users.noreply.github.com>
2025-05-14 14:47:37 +0200

    combinaciones_modificadas

--- Classificación_secuencias/Pruebas_Hui.py
+++ Classificación_secuencias/Pruebas_Hui.py
@@ -1,7 +1,20 @@
 import spacy
+import nltk
+from nltk.corpus import conll2002
+import spacy
+import matplotlib.pyplot as plt
+import string
+from nltk.tag import CRFTagger
+from itertools import combinations
+import time
+import pandas as pd
+import seaborn as sns
+train = conll2002.iob_sents('esp.train')
+
 
 class data:
-    def __init__(self, data):
+    def __init__(self, data, language="es"):
+        self._data = data
         self._words = []
         self._pos = []
         self._bio = []
@@ -10,57 +23,31 @@ class data:
                 self._words.append(word)
                 self._pos.append(pos)
                 self._bio.append(bio)
-
-    def __call__(self, language="es"):
-        # Load the Spanish model if not already loaded
+        
         if language == "es":
-            nlp = spacy.load("es_core_news_sm")
+            self._nlp = spacy.load("es_core_news_sm")
         elif language == "nl":
-            nlp = spacy.load("nl_core_news_sm")
+            self._nlp = spacy.load("nl_core_news_sm")
+        else:
+            raise ValueError("Unsupported language")
         
-        # Set a larger max_length if needed (but be careful with memory)
-        nlp.max_length = 1500000  # Increase if you're sure your system can handle it
-        
-        # Process in chunks if text is too large
-        chunk_size = 100000  # characters per chunk
+        self._nlp.max_length = 1500000
+        self.language = language
+    def lemmatize(self):
         lemmas = []
-        
-        # Split words into chunks and process each chunk
-        current_chunk = []
-        current_length = 0
-        
-        for word in self._words:
-            word_len = len(word) + 1  # +1 for space
-            if current_length + word_len > chunk_size:
-                # Process current chunk
-                chunk_text = " ".join(current_chunk)
-                doc = nlp(chunk_text)
-                chunk_lemmas = [token.lemma_ for token in doc]
-                lemmas.extend(chunk_lemmas)
+        # Process each sentence individually
+        for sentence in self._data:
+            # Extract just the words for lemmatization
+            words = [word for word, _, _ in sentence]
+            text = " ".join(words)
+            
+            # Process with SpaCy
+            doc = self._nlp(text)
+            
+            # Extract lemmas and add to the list
+            for token in doc:
+                lemmas.append(token.lemma_)
                 
-                # Start new chunk
-                current_chunk = [word]
-                current_length = word_len
-            else:
-                # Add to current chunk
-                current_chunk.append(word)
-                current_length += word_len
-        
-        # Process the last chunk if it's not empty
-        if current_chunk:
-            chunk_text = " ".join(current_chunk)
-            doc = nlp(chunk_text)
-            chunk_lemmas = [token.lemma_ for token in doc]
-            lemmas.extend(chunk_lemmas)
-        
-        # Ensure we have the same number of lemmas as words
-        if len(lemmas) != len(self._words):
-            # In case of mismatch (due to tokenization differences), fallback to simple lemmatization
-            lemmas = []
-            for word in self._words:
-                doc = nlp(word)
-                lemmas.append(doc[0].lemma_)
-        
         return lemmas
 
     def get_word_pos(self):
@@ -68,6 +55,30 @@ class data:
     
     def get_word_bio(self):
         return list(zip(self._words, self._bio))
+    def get_all_lemmas(self):
+        processed_sentences = []
+        
+        for sentence in self._data:
+            # Extract words for this sentence
+            words = [word for word, _, _ in sentence]
+            text = " ".join(words)
+            
+            # Process with SpaCy
+            doc = self._nlp(text)
+            
+            # Create processed sentence
+            processed_sentence = []
+            for i, (word, pos, bio) in enumerate(sentence):
+                if i < len(doc):
+                    lemma = doc[i].lemma_
+                    processed_sentence.append(((word, pos, lemma), bio))
+                else:
+                    # Fallback in case of token mismatch
+                    processed_sentence.append(((word, pos, word.lower()), bio))
+            
+            processed_sentences.append(processed_sentence)
+            
+        return processed_sentences
     
     def get_word(self):
         return self._words
@@ -76,18 +87,7 @@ class data:
     def get_bio(self):
         return self._bio
     
-class test_data:
-    def __init__(self, data):
-        self._words = []
-        self._pos = []
-        self._bio = []
-        for word, pos, bio in data:
-            self._words.append(word)
-            self._pos.append(pos)
-            self._bio.append(bio)
-    
-    def get_word_pos(self):
-        return list(zip(self._words, self._pos))
-    
-    def get_word_bio(self):
-        return list(zip(self._words, self._bio))
+train = data(train)
+print("===========================================================")
+train_lemmas = list(train.get_all_lemmas())
+print(train_lemmas[0:10])
